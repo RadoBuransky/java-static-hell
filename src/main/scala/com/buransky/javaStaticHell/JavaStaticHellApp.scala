4a24a518bb0f2f77fba93e3ad1dbaf7a16e76a0e
Rado Buransky <radoburansky@gmail.com>
2015-07-14 22:10:32 +0200

    Nice labeled DOT file

--- src/main/scala/com/buransky/javaStaticHell/JavaStaticHellApp.scala
+++ src/main/scala/com/buransky/javaStaticHell/JavaStaticHellApp.scala
@@ -1,10 +1,11 @@
 package com.buransky.javaStaticHell
 
-import java.io.File
+import java.io.{BufferedWriter, PrintWriter, File}
 import java.util.jar.JarFile
 import org.apache.bcel.classfile.ClassParser
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
 
 /**
  * Application command line arguments.
@@ -16,6 +17,8 @@ case class JavaStaticHellAppArgs(jarFilePath: File) {
     throw new IllegalArgumentException(s"JAR file does not exist! [$jarFilePath]")
 }
 
+case class Dep(from: String, to: Iterable[String])
+
 /**
  * Application entry point.
  */
@@ -23,6 +26,9 @@ object JavaStaticHellApp {
   def main(args: Array[String]) = {
     // Get application arguments
     val appArgs = getAppAgrs(args)
+
+    // Buffer with all static dependencies
+    val buffer = new ListBuffer[Dep]
 
     // Go through all entries
     val jarEntries = new JarFile(appArgs.jarFilePath).entries()
@@ -34,12 +40,51 @@ object JavaStaticHellApp {
         val javaClass = classParser.parse()
         val classVisitor = new ClassVisitor(javaClass)
         val deps = classVisitor.staticDependencies()
-        deps.foreach { dep =>
-          Console.out.println(s"${javaClass.getClassName} statically depends on $dep")
-        }
+        if (deps.nonEmpty)
+          buffer += Dep(javaClass.getClassName, deps)
       }
     }
+
+    // Generate DOT file
+    generateDotFile(new File("./build/graph.dot"), buffer)
   }
+
+  private def generateDotFile(dotFile: File, deps: Iterable[Dep]): Unit = {
+    val output = new BufferedWriter(new PrintWriter(dotFile))
+    try {
+      output.write("digraph javaStaticHell {\n")
+      try {
+        // First print all classes and label them
+        val all = deps.toSeq.flatMap(dep => dep.to.seq ++ Seq(dep.from)).distinct
+        all.foreach { dep =>
+          val dotName = classNameForDot(dep)
+          val dotLabel = labelForDot(dep)
+
+          output.write(s"    $dotName [label=$dotLabel];\n")
+        }
+
+        // And now print all dependencies
+        deps.foreach { dep =>
+          val fromName = classNameForDot(dep.from)
+          dep.to.foreach { depTo =>
+            val toName = classNameForDot(depTo)
+            output.write(s"    $fromName -> $toName;\n")
+          }
+        }
+      }
+      finally {
+        output.write("}\n")
+      }
+    }
+    finally {
+      output.close()
+    }
+
+    Console.out.println(s"DOT file generated. [${dotFile.getAbsolutePath}]")
+  }
+
+  private def classNameForDot(className: String): String = className.replace(".", "")
+  private def labelForDot(className: String): String = className.substring(className.lastIndexOf('.') + 1)
 
   private def getAppAgrs(args: Array[String]) =
     JavaStaticHellAppArgs(new File(args(0)))
